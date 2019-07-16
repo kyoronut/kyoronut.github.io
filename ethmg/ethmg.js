7963e25a0fcf43311013707776ecb021fb0aabc9
kyoronut <kyoronut@gmail.com>
2019-07-17 01:25:55 +0900

    wbtc usdc

--- ethmg/ethmg.js
+++ ethmg/ethmg.js
@@ -3,13 +3,13 @@ const am = [0.1, 1, 3, 10, 30, 100];
 var amount = document.getElementById("amount");
 var buy = document.getElementById("buy");
 var sell = document.getElementById("sell");
-document.getElementById("token").innerHTML = "DAI";
+var token = document.getElementById("token").innerHTML;
 
 function get_buy_eth_info(val){
 	console.log(val);
 	return new Promise(function(resolve){
 		$.ajax({
-			url:"https://api.dex.ag/trade?from=DAI&to=ETH&toAmount="+ String(val.toFixed(3)) +"&dex=best",
+			url:"https://api.dex.ag/trade?from=" + token + "&to=ETH&toAmount="+ String(val.toFixed(3)) +"&dex=best",
 			dataType:"json",
 		})
 
@@ -30,7 +30,7 @@ function get_sell_eth_info(val){
 	console.log(val);
 	return new Promise(function(resolve){
 		$.ajax({
-			url:"https://api.dex.ag/trade?from=ETH&to=DAI&fromAmount="+ String(val.toFixed(3)) +"&dex=best",
+			url:"https://api.dex.ag/trade?from=ETH&to=" + token + "&fromAmount="+ String(val.toFixed(3)) +"&dex=best",
 			dataType:"json",
 		})
 
@@ -52,7 +52,7 @@ for(var i = 0; i < n; i++){
 	list[i] = get_buy_eth_info(am[i]);
 	list[i + n] = get_sell_eth_info(am[i]);
 }
-	Promise.all(list)
+Promise.all(list)
 	.then(function(data){
 		for(var i = 0; i < n; i++){
 			var buy_price = Number(data[i][0]);
@@ -60,11 +60,19 @@ for(var i = 0; i < n; i++){
 			var spread = buy_price - sell_price;
 			var medium = (buy_price + sell_price) / 2;
 			var spread_p = spread / medium * 100;
-			buy.innerHTML += "<h4>" + String(Number(data[i][0]).toFixed(1)) + "</h4>"
-				+ "<h5>" + String(data[i][1]) + "</h5>";
-			sell.innerHTML += "<h4>" + String(Number(data[i + n][0]).toFixed(1)) + "</h4>"
-				+ "<h5>" + String(data[i + n][1]) + "</h5>";
-			
+			var digit;
+			if(buy_price > 100){
+				buy.innerHTML += "<h4>" + String(Number(data[i][0]).toFixed(1)) + "</h4>"
+					+ "<h5>" + String(data[i][1]) + "</h5>";
+				sell.innerHTML += "<h4>" + String(Number(data[i + n][0]).toFixed(1)) + "</h4>"
+					+ "<h5>" + String(data[i + n][1]) + "</h5>";
+			}else{
+				buy.innerHTML += "<h4>" + String(Number(data[i][0]).toExponential(3)) + "</h4>"
+					+ "<h5>" + String(data[i][1]) + "</h5>";
+				sell.innerHTML += "<h4>" + String(Number(data[i + n][0]).toExponential(3)) + "</h4>"
+					+ "<h5>" + String(data[i + n][1]) + "</h5>";
+			}
+
 			amount.innerHTML += "<h4>" + String(am[i]) + "</h4>"
 				+ "<h5>" + String(Number(spread_p).toFixed(2))+"%</h5>";
 		}
